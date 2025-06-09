ed95d7288f5b65c77f45833da8d0885ee2d245be
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-09 12:52:07 -0500

    refactos: Add EnableJpaAuditing in main

--- src/main/java/pe/edu/upc/center/agecare/AgecareCenterPlatformApplication.java
+++ src/main/java/pe/edu/upc/center/agecare/AgecareCenterPlatformApplication.java
@@ -2,8 +2,10 @@ package pe.edu.upc.center.agecare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AgecareCenterPlatformApplication {
 
 	public static void main(String[] args) {
