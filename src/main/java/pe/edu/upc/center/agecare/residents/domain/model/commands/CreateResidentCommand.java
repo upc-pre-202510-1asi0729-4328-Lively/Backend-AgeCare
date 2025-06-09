7686a87a46b2652cde6a2012cb05ba7c9e88a55e
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-09 13:55:56 -0500

    feat: Add new imports

--- src/main/java/pe/edu/upc/center/agecare/residents/domain/model/commands/CreateResidentCommand.java
+++ src/main/java/pe/edu/upc/center/agecare/residents/domain/model/commands/CreateResidentCommand.java
@@ -1,4 +1,17 @@
 package pe.edu.upc.center.agecare.residents.domain.model.commands;
 
-public record CreateResidentCommand() {
-}
+
+import pe.edu.upc.center.agecare.residents.domain.model.valueobjects.FullName;
+import pe.edu.upc.center.agecare.residents.domain.model.valueobjects.ReceiptId;
+import pe.edu.upc.center.agecare.residents.domain.model.valueobjects.Address;
+
+import java.util.Date;
+
+public record CreateResidentCommand(
+        String dni,
+        FullName fullName,
+        Address address,
+        Date birthDate,
+        String gender,
+        ReceiptId receipt
+) {}
