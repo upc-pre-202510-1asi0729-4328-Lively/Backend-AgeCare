ab62e6b3bc2e24887e9b66995b7fc81f09240bfd
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-09 12:40:14 -0500

    Primer commit

---
+++ src/main/java/pe/edu/upc/center/agecare/AgecareCenterPlatformApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.center.agecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgecareCenterPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgecareCenterPlatformApplication.class, args);
+	}
+
+}
