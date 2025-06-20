4f58a6bcc8e0b786c2b827d49dc300bf2ad4b265
FatimaAP05 <125381316+FatimaAP05@users.noreply.github.com>
2025-06-19 23:04:42 -0500

    feat: Add new transform

---
+++ src/main/java/pe/edu/upc/center/agecare/notification/interfaces.rest/transform/CreateNotificationCommandFromResourceAssembler.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.center.agecare.notification.interfaces.rest.transform;
+
+public class CreateNotificationCommandFromResourceAssembler {
+    // Assembler for creating notification commands from resources
+}
