6efda4d7c1dea3ca01390bc19c614d468a28e1e9
FatimaAP05 <125381316+FatimaAP05@users.noreply.github.com>
2025-06-19 23:08:49 -0500

    fix: documentation

--- src/main/java/pe/edu/upc/center/agecare/AgecareCenterPlatformApplication.java
+++ src/main/java/pe/edu/upc/center/agecare/AgecareCenterPlatformApplication.java
@@ -2,6 +2,7 @@ package pe.edu.upc.center.agecare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
